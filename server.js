3b4a332801cf9665b38004b1b7e89cffb7070a52
SAIIVARUN16 <112090025+SAIIVARUN16@users.noreply.github.com>
2023-10-28 12:05:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express=require('express')
-const app=express()
-app.listen('/',(req,res)=>{
-    console.log("Hello Saii Varun M R from AWS Virtual Server");
-})
-app.listen(3000)
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello Saii Varun M R from AWS Virtual Server");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
